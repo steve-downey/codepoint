bf71fdbeb3897eaefc57b2834914ebe4dcb5479a
Steve Downey <sdowney@gmail.com>
2019-03-03 18:29:05 -0500

    Move out of scratch

--- src/examples/main.cpp
+++ src/examples/main.cpp
@@ -1,10 +1,10 @@
-#include <scratch/scratch.h>
+#include <codepoint/codepoint.h>
 
 #include <iostream>
 
 int main(int /*argc*/, char** /*argv*/)
 {
-    using namespace scratch;
+    using namespace unicode;
 
 
 }
