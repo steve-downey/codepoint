21a9884af6cb46de67d9cd9255f53e681a29c1be
Steve Downey <sdowney@gmail.com>
2019-03-03 16:17:49 -0500

    Initial Codepoint

--- src/scratch/scratch.h
+++ src/scratch/scratch.h
@@ -2,13 +2,79 @@
 #ifndef INCLUDED_SCRATCH
 #define INCLUDED_SCRATCH
 
-#include <memory>
-#include <variant>
-#include <vector>
+#include <experimental/ranges/algorithm>
+#include <experimental/ranges/iterator>
+
+namespace ranges = std::experimental::ranges;
 
 namespace scratch {
 
+class codepoint {
+    char32_t value_;
 
+  public:
+    codepoint() = default;
+
+    template<ranges::Integral I>
+    constexpr codepoint(I i) : value_(i) {}
+
+    template<ranges::Integral I>
+    constexpr operator I() const {return  value_;}
+
+    codepoint& operator++();
+    codepoint& operator--();
+    codepoint operator++(int);
+    codepoint operator--(int);
+
+    friend constexpr bool operator==(codepoint l, codepoint r);
+    friend constexpr bool operator!=(codepoint l, codepoint r);
+    friend constexpr bool operator<(codepoint l, codepoint r);
+    friend constexpr bool operator<=(codepoint l, codepoint r);
+    friend constexpr bool operator>(codepoint l, codepoint r);
+    friend constexpr bool operator>=(codepoint l, codepoint r);
+
+    friend constexpr ptrdiff_t operator-(codepoint lhs, codepoint rhs);
+};
+
+codepoint& codepoint::operator++() {
+    ++value_;
+    return *this;
+}
+codepoint& codepoint::operator--() {
+    --value_;
+    return *this;
+}
+codepoint codepoint::operator++(int) {
+    char32_t tmp = value_++;
+    return tmp;
+}
+codepoint codepoint::operator--(int) {
+    char32_t tmp = value_--;
+    return tmp;
+}
+
+constexpr bool operator==(codepoint l, codepoint r) {
+    return l.value_ == r.value_;
+}
+constexpr bool operator!=(codepoint l, codepoint r) {
+    return l.value_ != r.value_;
+}
+constexpr bool operator<(codepoint l, codepoint r) {
+    return l.value_ < r.value_;
+}
+constexpr bool operator<=(codepoint l, codepoint r) {
+    return l.value_ <= r.value_;
+}
+constexpr bool operator>(codepoint l, codepoint r) {
+    return l.value_ > r.value_;
+}
+constexpr bool operator>=(codepoint l, codepoint r) {
+    return l.value_ >= r.value_;
+}
+
+constexpr ptrdiff_t operator-(codepoint lhs, codepoint rhs) {
+    return lhs.value_ - rhs.value_;
+}
 } // namespace scratch
 
 #endif
--- src/scratch/scratch.t.cpp
+++ src/scratch/scratch.t.cpp
@@ -9,5 +9,83 @@ TEST(ScratchTest, TestGTest) {
 }
 
 TEST(ScratchTest, Breathing) {
+    codepoint cp1;
+    codepoint cp2{0};
+    codepoint cp3{cp2};
 
+    codepoint cp4 = codepoint{};
+    codepoint cp5 = codepoint{1};
+
+    char32_t c32 = 0x1234;
+    codepoint cp6{c32};
+    codepoint cp7 = c32;
+    cp1 = cp2;
+    ASSERT_EQ(cp1, 0);
+    cp1 = c32;
+    ASSERT_EQ(cp1, 0x1234);
+
+    codepoint a = 'a';
+    ASSERT_EQ(a, 'a');
+
+    codepoint a8 = u8'a';
+    ASSERT_EQ(a8, 'a');
+
+}
+
+TEST(ScratchTest, IntegralCompare) {
+    constexpr codepoint cp1{1};
+
+    ASSERT_EQ(1, cp1);
+    ASSERT_NE(2, cp1);
+    ASSERT_EQ(cp1, 1);
+    ASSERT_NE(cp1, 2);
+}
+
+TEST(ScratchTest, IntegralAssign) {
+    constexpr codepoint cp1{1};
+    int i;
+    i = cp1;
+    ASSERT_EQ(1, i);
+}
+
+TEST(ScratchTest, IncDec) {
+    codepoint cp1;
+    cp1 = 1;
+    codepoint t1 = cp1++;
+    ASSERT_EQ(t1, 1);
+    ASSERT_EQ(cp1, 2);
+
+    codepoint t2 = ++cp1;
+    ASSERT_EQ(t2, 3);
+    ASSERT_EQ(cp1, 3);
+
+    codepoint t3 = cp1--;
+    ASSERT_EQ(t3, 3);
+    ASSERT_EQ(cp1, 2);
+
+    codepoint t4 = --cp1;
+    ASSERT_EQ(t4, 1);
+    ASSERT_EQ(cp1, 1);
+
+    codepoint& r1 = ++cp1;
+    codepoint& r2 = --cp1;
+}
+
+TEST(ScratchTest, Diff) {
+    constexpr codepoint cp1{1};
+    constexpr codepoint cp2{3};
+    constexpr ptrdiff_t diff = (cp2 - cp1);
+    ASSERT_EQ(diff, 2);
+}
+
+TEST(ScratchTest, Compare) {
+    constexpr codepoint cp1{1};
+    constexpr codepoint cp2{3};
+
+    ASSERT_TRUE((cp1 == cp1));
+    ASSERT_TRUE((cp1 != cp2));
+    ASSERT_TRUE((cp1 < cp2));
+    ASSERT_TRUE((cp1 <= cp1));
+    ASSERT_TRUE((cp2 > cp1));
+    ASSERT_TRUE((cp2 >= cp2));
 }
