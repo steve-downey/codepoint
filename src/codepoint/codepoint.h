31e465b88f35ca0f1e1f9942d42f60212e829bd0
Steve Downey <sdowney@gmail.com>
2019-03-03 23:33:50 -0500

    Noexcept all the things

--- src/codepoint/codepoint.h
+++ src/codepoint/codepoint.h
@@ -13,121 +13,121 @@ class codepoint {
     char32_t value_;
 
   public:
-    codepoint() = default;
+    codepoint() noexcept = default;
 
     template <ranges::Integral I>
-    constexpr codepoint(I i);
+    constexpr codepoint(I i) noexcept;
 
     template <ranges::Integral I>
-    constexpr explicit operator I() const;
+    constexpr explicit operator I() const noexcept;
 
-    codepoint& operator++();
-    codepoint& operator--();
-    codepoint  operator++(int);
-    codepoint  operator--(int);
-
-    template <ranges::Integral I>
-    codepoint& operator+=(I b);
+    codepoint& operator++() noexcept;
+    codepoint& operator--() noexcept;
+    codepoint  operator++(int) noexcept;
+    codepoint  operator--(int) noexcept;
 
     template <ranges::Integral I>
-    codepoint& operator-=(I b);
+    codepoint& operator+=(I b) noexcept;
 
     template <ranges::Integral I>
-    friend codepoint operator+(codepoint lhs, I rhs) {
+    codepoint& operator-=(I b) noexcept;
+
+    template <ranges::Integral I>
+    friend codepoint operator+(codepoint lhs, I rhs) noexcept {
         lhs += rhs;
         return lhs;
     }
 
     template <ranges::Integral I>
-    friend codepoint operator-(codepoint lhs, I rhs) {
+    friend codepoint operator-(codepoint lhs, I rhs) noexcept {
         lhs -= rhs;
         return lhs;
     }
 
     //  auto constexpr operator<=>(codepoint) = default;
-    friend constexpr bool operator==(codepoint l, codepoint r);
-    friend constexpr bool operator!=(codepoint l, codepoint r);
-    friend constexpr bool operator<(codepoint l, codepoint r);
-    friend constexpr bool operator<=(codepoint l, codepoint r);
-    friend constexpr bool operator>(codepoint l, codepoint r);
-    friend constexpr bool operator>=(codepoint l, codepoint r);
+    friend constexpr bool operator==(codepoint l, codepoint r) noexcept;
+    friend constexpr bool operator!=(codepoint l, codepoint r) noexcept;
+    friend constexpr bool operator<(codepoint l, codepoint r) noexcept;
+    friend constexpr bool operator<=(codepoint l, codepoint r) noexcept;
+    friend constexpr bool operator>(codepoint l, codepoint r) noexcept;
+    friend constexpr bool operator>=(codepoint l, codepoint r) noexcept;
 
-    friend constexpr ptrdiff_t operator-(codepoint lhs, codepoint rhs);
+    friend constexpr ptrdiff_t operator-(codepoint lhs,
+                                         codepoint rhs) noexcept;
 };
 
 template <class CharT, class Traits>
 inline std::basic_ostream<CharT, Traits>&
-operator<<(std::basic_ostream<CharT, Traits>& os, codepoint c) {
+operator<<(std::basic_ostream<CharT, Traits>& os, codepoint c) noexcept {
     return (os << static_cast<char32_t>(c));
 }
 
-
 // IMPLEMENTATIONS
 template <ranges::Integral I>
-inline constexpr codepoint::codepoint(I i) : value_(i) {}
+inline constexpr codepoint::codepoint(I i) noexcept : value_(i) {}
 
 template <ranges::Integral I>
-inline constexpr codepoint::operator I() const {
+inline constexpr codepoint::operator I() const noexcept {
     return value_;
 }
 
-inline codepoint& codepoint::operator++() {
+inline codepoint& codepoint::operator++() noexcept {
     ++value_;
     return *this;
 }
 
-inline codepoint& codepoint::operator--() {
+inline codepoint& codepoint::operator--() noexcept {
     --value_;
     return *this;
 }
 
-inline codepoint codepoint::operator++(int) {
+inline codepoint codepoint::operator++(int) noexcept {
     char32_t tmp = value_++;
     return tmp;
 }
 
-inline codepoint codepoint::operator--(int) {
+inline codepoint codepoint::operator--(int) noexcept {
     char32_t tmp = value_--;
     return tmp;
 }
 
 template <ranges::Integral I>
-inline codepoint& codepoint::operator+=(I i) {
+inline codepoint& codepoint::operator+=(I i) noexcept {
     value_ += i;
     return *this;
 }
 
 template <ranges::Integral I>
-inline codepoint& codepoint::operator-=(I i) {
+inline codepoint& codepoint::operator-=(I i) noexcept {
     value_ -= i;
     return *this;
 }
 
-inline constexpr bool operator==(codepoint l, codepoint r) {
+inline constexpr bool operator==(codepoint l, codepoint r) noexcept {
     return l.value_ == r.value_;
 }
 
-inline constexpr bool operator!=(codepoint l, codepoint r) {
+inline constexpr bool operator!=(codepoint l, codepoint r) noexcept {
     return l.value_ != r.value_;
 }
 
-inline constexpr bool operator<(codepoint l, codepoint r) {
+inline constexpr bool operator<(codepoint l, codepoint r) noexcept {
     return l.value_ < r.value_;
 }
 
-inline constexpr bool operator<=(codepoint l, codepoint r) {
+inline constexpr bool operator<=(codepoint l, codepoint r) noexcept {
     return l.value_ <= r.value_;
 }
 
-inline constexpr bool operator>(codepoint l, codepoint r) {
+inline constexpr bool operator>(codepoint l, codepoint r) noexcept {
     return l.value_ > r.value_;
 }
 
-inline constexpr bool operator>=(codepoint l, codepoint r) {
+inline constexpr bool operator>=(codepoint l, codepoint r) noexcept {
     return l.value_ >= r.value_;
 }
 
-inline constexpr ptrdiff_t operator-(codepoint lhs, codepoint rhs) {
+inline constexpr ptrdiff_t operator-(codepoint lhs, codepoint rhs) noexcept {
     return lhs.value_ - rhs.value_;
 }
 
