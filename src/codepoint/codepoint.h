f568619e7bf61f2f91ebc178cf54ef75c3c86960
Steve Downey <sdowney@gmail.com>
2019-03-04 21:19:44 -0500

    Codepoint as enum class

--- src/codepoint/codepoint.h
+++ src/codepoint/codepoint.h
@@ -9,131 +9,13 @@ namespace ranges = std::experimental::ranges;
 
 namespace unicode {
 
-class codepoint {
-    char32_t value_; // for exposition only
-
-  public:
-    codepoint() noexcept = default;
-
-    template <ranges::Integral I>
-    constexpr codepoint(I i) noexcept;
-
-    template <ranges::Integral I>
-    constexpr explicit operator I() const noexcept;
-
-    constexpr codepoint& operator++() noexcept;
-    constexpr codepoint& operator--() noexcept;
-    constexpr codepoint  operator++(int) noexcept;
-    constexpr codepoint  operator--(int) noexcept;
-
-    template <ranges::Integral I>
-    constexpr codepoint& operator+=(I b) noexcept;
-
-    template <ranges::Integral I>
-    constexpr codepoint& operator-=(I b) noexcept;
-
-    template <ranges::Integral I>
-    friend constexpr codepoint operator+(codepoint lhs, I rhs) noexcept {
-        lhs += rhs;
-        return lhs;
-    }
-
-    template <ranges::Integral I>
-    friend constexpr codepoint operator-(codepoint lhs, I rhs) noexcept {
-        lhs -= rhs;
-        return lhs;
-    }
-
-    template <ranges::Integral I>
-    friend constexpr codepoint operator+(I lhs, codepoint rhs) noexcept {
-        lhs += static_cast<I>(rhs);
-        return lhs;
-    }
-
-    template <ranges::Integral I>
-    friend constexpr codepoint operator-(I lhs, codepoint rhs) noexcept {
-        lhs -= static_cast<I>(rhs);
-        return lhs;
-    }
-
-    //  auto constexpr operator<=>(codepoint) = default;
-    friend constexpr bool operator==(codepoint l, codepoint r) noexcept{
-        return l.value_ == r.value_;
-    }
-
-    friend constexpr bool operator!=(codepoint l, codepoint r) noexcept{
-        return l.value_ != r.value_;
-    }
-
-    friend constexpr bool operator<(codepoint l, codepoint r) noexcept{
-        return l.value_ < r.value_;
-    }
-
-    friend constexpr bool operator<=(codepoint l, codepoint r) noexcept{
-        return l.value_ <= r.value_;
-    }
-
-    friend constexpr bool operator>(codepoint l, codepoint r) noexcept{
-        return l.value_ > r.value_;
-    }
-
-    friend constexpr bool operator>=(codepoint l, codepoint r) noexcept{
-        return l.value_ >= r.value_;
-    }
-
-    friend constexpr int_least32_t operator-(codepoint lhs,
-                                             codepoint rhs) noexcept{
-        return lhs.value_ - rhs.value_;
-    }
+enum class codepoint : char32_t {
 };
 
-template <class CharT, class Traits>
-inline std::basic_ostream<CharT, Traits>&
-operator<<(std::basic_ostream<CharT, Traits>& os, codepoint c) noexcept {
-    return (os << static_cast<char32_t>(c));
-}
-
-// IMPLEMENTATIONS
 template <ranges::Integral I>
-inline constexpr codepoint::codepoint(I i) noexcept : value_(i) {}
-
-template <ranges::Integral I>
-inline constexpr codepoint::operator I() const noexcept {
-    return value_;
+constexpr I to_integer(codepoint c) noexcept {
+    return I(c);
 }
-
-inline constexpr codepoint& codepoint::operator++() noexcept {
-    ++value_;
-    return *this;
-}
-
-inline constexpr codepoint& codepoint::operator--() noexcept {
-    --value_;
-    return *this;
-}
-
-inline constexpr codepoint codepoint::operator++(int) noexcept {
-    char32_t tmp = value_++;
-    return tmp;
-}
-
-inline constexpr codepoint codepoint::operator--(int) noexcept {
-    char32_t tmp = value_--;
-    return tmp;
-}
-
-template <ranges::Integral I>
-inline constexpr codepoint& codepoint::operator+=(I i) noexcept {
-    value_ += i;
-    return *this;
-}
-
-template <ranges::Integral I>
-inline constexpr codepoint& codepoint::operator-=(I i) noexcept {
-    value_ -= i;
-    return *this;
-}
-
 
 } // namespace unicode
 
--- src/codepoint/codepoint.t.cpp
+++ src/codepoint/codepoint.t.cpp
@@ -20,27 +20,27 @@ TEST(CodepointTest, Breathing) {
 
     char32_t c32 = 0x1234;
     codepoint cp6{c32};
-    codepoint cp7 = c32;
+    codepoint cp7 = codepoint(c32);
     cp1 = cp2;
-    ASSERT_EQ(cp1, 0);
-    cp1 = c32;
-    ASSERT_EQ(cp1, 0x1234);
+    ASSERT_EQ(cp1, codepoint(0));
+    cp1 = codepoint(c32);
+    ASSERT_EQ(cp1, codepoint(0x1234));
 
-    codepoint a = 'a';
-    ASSERT_EQ(a, 'a');
+    codepoint a = codepoint('a');
+    ASSERT_EQ(a, codepoint('a'));
 
-    codepoint a8 = u8'a';
-    ASSERT_EQ(a8, 'a');
+    codepoint a8 = codepoint(u8'a');
+    ASSERT_EQ(a8, codepoint('a'));
 
 }
 
 TEST(CodepointTest, IntegralCompare) {
     constexpr codepoint cp1{1};
 
-    ASSERT_EQ(1, cp1);
-    ASSERT_NE(2, cp1);
-    ASSERT_EQ(cp1, 1);
-    ASSERT_NE(cp1, 2);
+    ASSERT_EQ(codepoint(1), cp1);
+    ASSERT_NE(codepoint(2), cp1);
+    ASSERT_EQ(cp1, codepoint(1));
+    ASSERT_NE(cp1, codepoint(2));
 }
 
 TEST(CodepointTest, IntegralAssign) {
@@ -50,100 +50,100 @@ TEST(CodepointTest, IntegralAssign) {
     ASSERT_EQ(1, i);
 }
 
-TEST(CodepointTest, AddSubAssign) {
-    codepoint cp1{1};
-    codepoint cp2 = (cp1 += 1);
-    ASSERT_EQ(cp1, 2);
-    ASSERT_EQ(cp2, cp1);
-    codepoint cp3 = (cp2 -= 2);
-    ASSERT_EQ(cp3, 0);
-    ASSERT_EQ(cp3, cp2);
+// TEST(CodepointTest, AddSubAssign) {
+//     codepoint cp1{1};
+//     codepoint cp2 = (cp1 += 1);
+//     ASSERT_EQ(cp1, 2);
+//     ASSERT_EQ(cp2, cp1);
+//     codepoint cp3 = (cp2 -= 2);
+//     ASSERT_EQ(cp3, 0);
+//     ASSERT_EQ(cp3, cp2);
 
-    codepoint cp4 = 0;
-    (cp4 += 1) += 2;
-    ASSERT_EQ(cp4, 3);
-}
+//     codepoint cp4 = 0;
+//     (cp4 += 1) += 2;
+//     ASSERT_EQ(cp4, 3);
+// }
 
-TEST(CodepointTest, ConstExprAddSubAssign) {
-    constexpr codepoint cp2 = (codepoint{1} += 1);
-    ASSERT_EQ(cp2, 2);
-    constexpr codepoint cp3 = (codepoint{2} -= 2);
-    ASSERT_EQ(cp3, 0);
-}
+// TEST(CodepointTest, ConstExprAddSubAssign) {
+//     constexpr codepoint cp2 = (codepoint{1} += 1);
+//     ASSERT_EQ(cp2, 2);
+//     constexpr codepoint cp3 = (codepoint{2} -= 2);
+//     ASSERT_EQ(cp3, 0);
+// }
 
 TEST(CodepointTest, AddSub) {
     codepoint cp1{1};
-    codepoint cp2 = (cp1 + 1);
-    ASSERT_EQ(cp1, 1);
-    ASSERT_EQ(cp2, 2);
-    codepoint cp3 = (cp2 - 2);
-    ASSERT_EQ(cp3, 0);
-    ASSERT_EQ(cp2, 2);
+    codepoint cp2 = codepoint(to_integer<int>(cp1) + 1);
+    ASSERT_EQ(cp1, codepoint(1));
+    ASSERT_EQ(cp2, codepoint(2));
+    codepoint cp3 = codepoint(char32_t(cp2) - 2);
+    ASSERT_EQ(cp3, codepoint(0));
+    ASSERT_EQ(cp2, codepoint(2));
 
-    codepoint cp4 = (cp3 + 2) + 1;
-    ASSERT_EQ(cp4, 3);
+    codepoint cp4 = codepoint((to_integer<char32_t>(cp3) + 2) + 1);
+    ASSERT_EQ(cp4, codepoint(3));
 
-    codepoint cp5 = (1 + cp1);
-    ASSERT_EQ(cp5, 2);
+    codepoint cp5 = codepoint(1 + to_integer<int>(cp1));
+    ASSERT_EQ(cp5, codepoint(2));
 
-    codepoint cp6 = (1 - cp1);
-    ASSERT_EQ(cp6, 0);
+    codepoint cp6 = codepoint(1 - to_integer<int>(cp1));
+    ASSERT_EQ(cp6, codepoint(0));
 }
 
 TEST(CodepointTest, ConstExprAddSub) {
     constexpr codepoint cp1{1};
-    constexpr codepoint cp2 = (cp1 + 1);
-    ASSERT_EQ(cp1, 1);
-    ASSERT_EQ(cp2, 2);
-    constexpr codepoint cp3 = (cp2 - 2);
-    ASSERT_EQ(cp3, 0);
-    ASSERT_EQ(cp2, 2);
+    constexpr codepoint cp2 = codepoint(to_integer<int>(cp1) + 1);
+    ASSERT_EQ(cp1, codepoint(1));
+    ASSERT_EQ(cp2, codepoint(2));
+    constexpr codepoint cp3 = codepoint(to_integer<int>(cp2) - 2);
+    ASSERT_EQ(cp3, codepoint(0));
+    ASSERT_EQ(cp2, codepoint(2));
 
-    constexpr codepoint cp4 = (cp3 + 2) + 1;
-    ASSERT_EQ(cp4, 3);
+    constexpr codepoint cp4 = codepoint((to_integer<int>(cp3) + 2) + 1);
+    ASSERT_EQ(cp4, codepoint(3));
 }
 
-TEST(CodepointTest, IncDec) {
-    codepoint cp1;
-    cp1 = 1;
-    codepoint t1 = cp1++;
-    ASSERT_EQ(t1, 1);
-    ASSERT_EQ(cp1, 2);
+// TEST(CodepointTest, IncDec) {
+//     codepoint cp1;
+//     cp1 = 1;
+//     codepoint t1 = cp1++;
+//     ASSERT_EQ(t1, 1);
+//     ASSERT_EQ(cp1, 2);
 
-    codepoint t2 = ++cp1;
-    ASSERT_EQ(t2, 3);
-    ASSERT_EQ(cp1, 3);
+//     codepoint t2 = ++cp1;
+//     ASSERT_EQ(t2, 3);
+//     ASSERT_EQ(cp1, 3);
 
-    codepoint t3 = cp1--;
-    ASSERT_EQ(t3, 3);
-    ASSERT_EQ(cp1, 2);
+//     codepoint t3 = cp1--;
+//     ASSERT_EQ(t3, 3);
+//     ASSERT_EQ(cp1, 2);
 
-    codepoint t4 = --cp1;
-    ASSERT_EQ(t4, 1);
-    ASSERT_EQ(cp1, 1);
+//     codepoint t4 = --cp1;
+//     ASSERT_EQ(t4, 1);
+//     ASSERT_EQ(cp1, 1);
 
-    codepoint& r1 = ++cp1;
-    codepoint& r2 = --cp1;
-}
+//     codepoint& r1 = ++cp1;
+//     codepoint& r2 = --cp1;
+// }
 
-TEST(CodepointTest, ConstExprIncDec) {
-    constexpr codepoint t1 = codepoint{4}++;
-    ASSERT_EQ(t1, 4);
+// TEST(CodepointTest, ConstExprIncDec) {
+//     constexpr codepoint t1 = codepoint{4}++;
+//     ASSERT_EQ(t1, 4);
 
-    constexpr codepoint t2 = ++codepoint{4};
-    ASSERT_EQ(t2, 5);
+//     constexpr codepoint t2 = ++codepoint{4};
+//     ASSERT_EQ(t2, 5);
 
-    constexpr codepoint t3 = codepoint{4}--;
-    ASSERT_EQ(t3, 4);
+//     constexpr codepoint t3 = codepoint{4}--;
+//     ASSERT_EQ(t3, 4);
 
-    constexpr codepoint t4 = --codepoint{4};
-    ASSERT_EQ(t4, 3);
-}
+//     constexpr codepoint t4 = --codepoint{4};
+//     ASSERT_EQ(t4, 3);
+// }
 
 TEST(CodepointTest, Diff) {
     constexpr codepoint cp1{1};
     constexpr codepoint cp2{3};
-    constexpr ptrdiff_t diff = (cp2 - cp1);
+    constexpr ptrdiff_t diff = (char32_t(cp2) - char32_t(cp1));
     ASSERT_EQ(diff, 2);
 }
 
@@ -162,7 +162,7 @@ TEST(CodepointTest, Compare) {
 TEST(CodepointTest, Stream) {
     std::ostringstream stream;
     constexpr codepoint cp1{'b'};
-    stream << cp1;
+    stream << char32_t(cp1);
     //   ASSERT_EQ(stream.str(), "b");
     ASSERT_EQ(stream.str(), "98");
 }
